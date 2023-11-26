e43ac3bdea5ceecdf582df11fa65615359559ab0
Michael <68960336+Mikewon98@users.noreply.github.com>
2023-11-26 12:44:42 +0300

    final commit

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -5,6 +5,7 @@ const initialState = {
   user: null,
   token: null,
   posts: [],
+  friends: [],
 };
 
 export const authSlice = createSlice({
@@ -26,7 +27,7 @@ export const authSlice = createSlice({
       if (state.user) {
         state.user.friends = action.payload.friends;
       } else {
-        console.error("User friends non-existent!");
+        console.error("user friends non-existent :(");
       }
     },
     setPosts: (state, action) => {
@@ -34,7 +35,7 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
